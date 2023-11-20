eb1dc09455ec48b84423d3f513c5b95dde141695
thomasvloo <vanloo.thomas@yahoo.com>
2023-11-20 16:19:58 +0100

    remove unused code and add comments to controller functions

--- Fabric-API/src/controllers/contractController.js
+++ Fabric-API/src/controllers/contractController.js
@@ -6,26 +6,36 @@ const {
     createAsset,
     assetExists,
     exportLedgerToCSV,
-    //readAsset,
-    //updateAsset,
-    //transferAsset,
 } = require("../scripts/chaincodeFunctions");
 
+/**
+ * Request Type: POST
+ * Request Path: /contract/initLedger
+ * Request parameters: -
+ * Description: Initializes the ledger with some pre-prepared log events.
+ */
 const initializeLedger = asyncHandler(async (req, res) => {
     const result = await initLedger();
     res.status(200).json(result);
 });
 
+/**
+ * Request Type: GET
+ * Request Path: /contract/getAllAssets
+ * Request parameters: -
+ * Description: Fetches and returns all logs currently in the ledger.
+ */
 const retrieveAllAssets = asyncHandler(async (req, res) => {
     const result = await getAllAssets();
     res.status(200).json(JSON.parse(result));
 });
 
-const saveledger = asyncHandler(async (req, res) => {
-    await exportLedgerToCSV();
-    res.status(200).send("Ledger saved to ledger.csv");
-});
-
+/**
+ * Request Type: POST
+ * Request Path: /contract/createAsset
+ * Request parameters: The log event to be stored on-chain in the request body
+ * Description: Takes the given log attributes and stores the log event on-chain.
+ */
 const createAnAsset = asyncHandler(async (req, res) => {
     console.log(req.body);
     const { eventId } = req.body;
@@ -34,35 +44,28 @@ const createAnAsset = asyncHandler(async (req, res) => {
     res.status(200).json(JSON.parse(result));
 });
 
+/**
+ * Request Type: GET
+ * Request Path: /contract/checkAssetExists
+ * Request parameters: the eventId of the log event
+ * Description: Returns true or false based on whether an event log exists with the given ID on-chain.
+ */
 const checkAssetExists = asyncHandler(async (req, res) => {
     const { assetID } = req.body;
     const result = await assetExists(assetID);
     res.status(200).json(JSON.parse(result));
 });
 
-// const readAnAsset = asyncHandler(async (req, res) => {
-//     const { assetID } = req.body;
-//     const result = await readAsset(assetID);
-//     res.status(200).json(JSON.parse(result));
-// });
-
-// const updateAnAsset = asyncHandler(async (req, res) => {
-//     const { assetID, color, size, owner, appraisedValue } = req.body;
-//     const result = await updateAsset(
-//         assetID,
-//         color,
-//         size,
-//         owner,
-//         appraisedValue
-//     );
-//     res.status(200).json(JSON.parse(result));
-// });
-
-// const transferAnAsset = asyncHandler(async (req, res) => {
-//     const { assetID, newOwner } = req.body;
-//     const result = await transferAsset(assetID, newOwner);
-//     res.status(200).json(JSON.parse(result));
-// });
+/**
+ * Request Type: GET
+ * Request Path: contract/saveLedger
+ * Request parameters: -
+ * Description: Fetches the entire ledger, formats it, and saves it as a csv file on the machine hosting the api
+ */
+const saveledger = asyncHandler(async (req, res) => {
+    await exportLedgerToCSV();
+    res.status(200).send("Ledger saved to ledger.csv");
+});
 
 module.exports = {
     initializeLedger,
@@ -70,7 +73,4 @@ module.exports = {
     createAnAsset,
     checkAssetExists,
     saveledger,
-    //readAnAsset,
-    //updateAnAsset,
-    //transferAnAsset,
 };

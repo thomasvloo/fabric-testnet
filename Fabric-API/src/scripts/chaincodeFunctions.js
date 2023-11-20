5a9661c8d7c0295b3c327f3d254fd072098ec158
thomasvloo <vanloo.thomas@yahoo.com>
2023-11-20 15:55:03 +0100

    modify chaincode to accept arbitrary log data

--- Fabric-API/src/scripts/chaincodeFunctions.js
+++ Fabric-API/src/scripts/chaincodeFunctions.js
@@ -25,38 +25,27 @@ async function getAllAssets() {
     return result.toString();
 }
 
-async function createAsset(assetID, color, size, owner, appraisedValue) {
+async function createAsset(params) {
     console.log(
         "\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments"
     );
+    console.log(params);
+    const { eventId } = params;
+    console.log(eventId);
+    const test = !eventId;
+    console.log(test);
     await networkManager.contract.submitTransaction(
         "CreateAsset",
-        assetID,
-        color,
-        size,
-        owner,
-        appraisedValue
+        JSON.stringify(params)
     );
     console.log("*** Result: committed");
     const result = await networkManager.contract.evaluateTransaction(
         "ReadAsset",
-        assetID
+        params.eventId
     );
     if (`${result}` !== "") {
         console.log(`*** Result: ${prettyJSONString(result.toString())}`);
     }
-    return result.toString();
-}
-
-async function readAsset(assetID) {
-    console.log(
-        "\n--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID"
-    );
-    const result = await networkManager.contract.evaluateTransaction(
-        "ReadAsset",
-        assetID
-    );
-    console.log(`*** Result: ${prettyJSONString(result.toString())}`);
     return result.toString();
 }
 
@@ -66,41 +55,6 @@ async function assetExists(assetID) {
     );
     const result = await networkManager.contract.evaluateTransaction(
         "AssetExists",
-        assetID
-    );
-    console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-    return result.toString();
-}
-
-async function updateAsset(assetID, color, size, owner, appraisedValue) {
-    console.log(`\n--> Submit Transaction: UpdateAsset ${assetID}`);
-    await networkManager.contract.submitTransaction(
-        "UpdateAsset",
-        assetID,
-        color,
-        size,
-        owner,
-        appraisedValue
-    );
-    const result = await networkManager.contract.evaluateTransaction(
-        "ReadAsset",
-        assetID
-    );
-    console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-    return result.toString();
-}
-
-async function transferAsset(assetID, newOwner) {
-    console.log(
-        `\n--> Submit Transaction: TransferAsset ${assetID}, transfer to new owner of ${newOwner}`
-    );
-    await networkManager.contract.submitTransaction(
-        "TransferAsset",
-        assetID,
-        newOwner
-    );
-    const result = await networkManager.contract.evaluateTransaction(
-        "ReadAsset",
         assetID
     );
     console.log(`*** Result: ${prettyJSONString(result.toString())}`);
@@ -141,52 +95,60 @@ async function exportLedgerToCSV() {
 
     return result.toString();
 }
-// pre-requisites:
-// - fabric-sample two organization test-network setup with two peers, ordering service,
-//   and 2 certificate authorities
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh up createChannel -ca
-// - Use any of the asset-transfer-basic chaincodes deployed on the channel "mychannel"
-//   with the chaincode name of "basic". The following deploy command will package,
-//   install, approve, and commit the javascript chaincode, all the actions it takes
-//   to deploy a chaincode to a channel.
-//         ===> from directory /fabric-samples/test-network
-//         ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript/ -ccl javascript
-// - Be sure that node.js is installed
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         node -v
-// - npm installed code dependencies
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         npm install
-// - to run this test application
-//         ===> from directory /fabric-samples/asset-transfer-basic/application-javascript
-//         node app.js
+// async function readAsset(assetID) {
+//     console.log(
+//         "\n--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID"
+//     );
+//     const result = await networkManager.contract.evaluateTransaction(
+//         "ReadAsset",
+//         assetID
+//     );
+//     console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+//     return result.toString();
+// }
 
-// NOTE: If you see  kind an error like these:
-/*
-    2020-08-07T20:23:17.590Z - error: [DiscoveryService]: send[mychannel] - Channel:mychannel received discovery error:access denied
-    ******** FAILED to run the application: Error: DiscoveryService: mychannel error: access denied
+// async function updateAsset(assetID, color, size, owner, appraisedValue) {
+//     console.log(`\n--> Submit Transaction: UpdateAsset ${assetID}`);
+//     await networkManager.contract.submitTransaction(
+//         "UpdateAsset",
+//         assetID,
+//         color,
+//         size,
+//         owner,
+//         appraisedValue
+//     );
+//     const result = await networkManager.contract.evaluateTransaction(
+//         "ReadAsset",
+//         assetID
+//     );
+//     console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+//     return result.toString();
+// }
 
-   OR
-
-   Failed to register user : Error: fabric-ca request register failed with errors [[ { code: 20, message: 'Authentication failure' } ]]
-   ******** FAILED to run the application: Error: Identity not found in wallet: appUser
-*/
-// Delete the /fabric-samples/asset-transfer-basic/application-javascript/wallet directory
-// and retry this application.
-//
-// The certificate authority must have been restarted and the saved certificates for the
-// admin and application user are not valid. Deleting the wallet store will force these to be reset
-// with the new certificate authority.
-//
+// async function transferAsset(assetID, newOwner) {
+//     console.log(
+//         `\n--> Submit Transaction: TransferAsset ${assetID}, transfer to new owner of ${newOwner}`
+//     );
+//     await networkManager.contract.submitTransaction(
+//         "TransferAsset",
+//         assetID,
+//         newOwner
+//     );
+//     const result = await networkManager.contract.evaluateTransaction(
+//         "ReadAsset",
+//         assetID
+//     );
+//     console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+//     return result.toString();
+// }
 
 module.exports = {
     initLedger,
     getAllAssets,
     createAsset,
-    readAsset,
     assetExists,
-    updateAsset,
-    transferAsset,
     exportLedgerToCSV,
+    //readAsset,
+    //updateAsset,
+    //transferAsset,
 };
